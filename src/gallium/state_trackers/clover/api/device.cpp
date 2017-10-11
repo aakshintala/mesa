762f58dd63dc1494fe02550ef0c708f4414492a6
Hangchen Yu <hyu@cs.utexas.edu>
2017-10-11 01:04:40 -0500

    Pass the compliation but can't start server

--- src/gallium/state_trackers/clover/api/device.cpp
+++ src/gallium/state_trackers/clover/api/device.cpp
@@ -26,6 +26,8 @@
 #include "git_sha1.h"
 #include "pipe/p_screen.h"
 
+#include "rpc.h"
+
 using namespace clover;
 
 CLOVER_API cl_int
@@ -95,6 +97,22 @@ clReleaseDevice(cl_device_id d_dev) try {
    return e.get();
 }
 
+class RemoteOpenCLServiceImpl final : public RemoteOpenCL::Service {
+    Status RemoteExec(ServerContext* context, const Call* call,
+        Response* response) override
+    {
+        response->set_func(call->func());
+        response->set_id(call->id());
+
+        switch (call->id()) {
+            default:
+                break;
+        }
+
+        return Status::OK;
+    }
+};
+
 CLOVER_API cl_int
 clGetDeviceInfo(cl_device_id d_dev, cl_device_info param,
                 size_t size, void *r_buf, size_t *r_size) try {
@@ -107,7 +125,18 @@ clGetDeviceInfo(cl_device_id d_dev, cl_device_info param,
        std::cout << "Para-virt server starts\n";
        std::cout << "Platform has " << dev.platform.devs.size()
            << " devices" << std::endl;
-       while (1);
+
+       // start RPC server
+       std::string server_address("0.0.0.0:88888");
+       RemoteOpenCLServiceImpl service;
+
+       ServerBuilder builder;
+       builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+       builder.RegisterService(&service);
+       std::unique_ptr<Server> server(builder.BuildAndStart());
+       std::cout << "Server listening on " << server_address << std::endl;
+
+       server->Wait();
    }
 
    /* para-virt server ends */
